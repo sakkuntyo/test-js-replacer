289112f09736cb787da6eee685290e79788d89e0
Daisuke Sasaki <dasasaki@microsoft.com>
2021-11-07 15:56:41 +0900

    replacer を使わないサンプル

---
+++ index.js
@@ -0,0 +1,21 @@
+let sampleobject = [
+	{
+		name: "dasasaki",
+		sex: "m"
+	},
+	{
+		name: "kentoyo",
+		sex: "m"
+	},
+	{
+		name: "tatsumiy",
+		sex: "m"
+	},
+	{
+		name: "yoheiu",
+		sex: "m"
+	}
+]
+
+// replacer を使わない
+console.log(JSON.stringify(sampleobject,null,2))
